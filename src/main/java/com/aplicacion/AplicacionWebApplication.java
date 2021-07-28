419d573dec01e52ad817d5152b21a099d4164be3
wilmerG123 <garnicawilmer.com>
2021-07-27 22:16:49 -0500

    subiendo proyecto

---
+++ src/main/java/com/aplicacion/AplicacionWebApplication.java
@@ -0,0 +1,13 @@
+package com.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplicacion/AplicacionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
